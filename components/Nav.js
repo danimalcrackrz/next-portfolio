20cc7be95f5b43e09869a5025b4c6c8c6c47dea8
Dan Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2022-06-21 20:44:06 -0500

    -added home link to nav -adjusted portfolio styles

--- components/Nav.js
+++ components/Nav.js
@@ -2,6 +2,7 @@ import Link from "next/link"
 import React from "react"
 import { DiGithubFull } from "react-icons/di"
 import { SiReact } from "react-icons/si"
+import { CgHomeAlt } from "react-icons/cg"
 
 const Nav = () => {
   return (
@@ -13,6 +14,9 @@ const Nav = () => {
         <p className='nav-title'>Daniel Shaw</p>
       </a>
       <ul className='nav-links'>
+        <Link href='/'>
+          <CgHomeAlt className='home-icon' />
+        </Link>
         <Link href='/portfolio'>Projects</Link>
         <Link href='/Contact'>Contact</Link>
         <Link href='/Source'>
